c32655d7b1f0a12b1d7c1ab2f0db57154daefba1
Paul Susanin <pasha.susanin15521552@mail.ru>
2021-08-09 15:08:58 +0300

    feat(array, while): make array from 1 to N using while and array

---
+++ array.php
@@ -0,0 +1,12 @@
+<?php
+
+$test_array = array(1,2);
+
+$number = readline("Write your number here: ");
+
+while (count($test_array) != $number)
+{
+    array_push($test_array, count($test_array) + 1);
+}
+
+print_r($test_array);
